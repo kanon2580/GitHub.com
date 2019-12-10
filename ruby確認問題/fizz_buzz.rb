c30e791aa67f8663c1eedfcec3ef715e7ae32736
kanon2580 <kanon.ueno1117@gmail.com>
2019-12-10 20:24:27 +0900

    [finish]2-9

---
+++ ruby確認問題/fizz_buzz.rb
@@ -0,0 +1,19 @@
+# coding:utf-8
+STDOUT.sync = true
+
+def fizz_buzz(number)
+	if number % 3 == 0 && number % 5 == 0
+		"FizzBuzz"
+	elsif number % 3 ==0
+		"Fizz"
+	elsif number % 5 ==0
+		"Buzz"
+	else
+		puts number.to_s
+	end
+end
+
+puts "1 以上の数字を入力してください"
+	i = gets.to_i
+puts "結果は、、、"
+puts fizz_buzz(i)
